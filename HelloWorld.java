c63d2c25573397ec854cb92e7369c9517f95e730
Josh Stoneking <joshstoneking2@gmail.com>
2025-04-07 14:18:23 -0700

    made a new new change

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,12 @@ public class HelloWorld {
         System.out.println("Hello, World!");
         // Duck!
         System.out.println("🦆");
+        
+        boolean isDuck = true;
+        if (isDuck) {
+            System.out.println("It's a duck!");
+        } else {
+            System.out.println("It's not a duck.");
+        }
     }
 }
