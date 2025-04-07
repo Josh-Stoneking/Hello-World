806d434e07db5e0d0b75d06e1095cb228fdcd40a
Josh Stoneking <joshstoneking2@gmail.com>
2025-04-07 13:55:30 -0700

    made requested changes

--- newFile.java
+++ newFile.java
@@ -1,6 +1,7 @@
 public class newFile {
     public static void main(String[] args) {
-        System.out.println("This is a new file made by Josh");
-        // coment made by josh
+      // this class is a basic java method in "hello world" style
+      System.out.println("This is a new file made by Josh");
+        // comment made by josh
     }
 }
